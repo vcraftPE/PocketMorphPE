fe24e91e98f94a0cd218348f6e4ce50f41bff424
VCraft <cubecraftpe@gmail.com>
2016-07-17 22:54:30 +0300

    Added the new mobs
    Horse , and the new mobs

---
+++ src/pocketmorph/morph/MorphWitch.php
@@ -0,0 +1,37 @@
+<?php
+namespace pocketmorph\morph;
+
+use pocketmine\network\protocol\AddEntityPacket;
+use pocketmine\Player;
+
+class Witch extends Morph implements MorphEntity
+{
+
+    const NETWORK_ID = 45;
+
+    public function getName()
+    {
+        return "Witch";
+    }
+
+    public function spawnTo(Player $player)
+    {
+
+        $pk = new AddEntityPacket();
+        $pk->eid = $this->getId();
+        $pk->type = self::NETWORK_ID;
+        $pk->x = $this->x;
+        $pk->y = $this->y;
+        $pk->z = $this->z;
+        $pk->yaw = $this->yaw;
+        $pk->pitch = $this->pitch;
+        $pk->metadata = [
+            3 => [0, $this->getDataProperty(3)],
+            15 => [0, 1]
+        ];
+        $player->dataPacket($pk);
+        parent::spawnTo($player);
+    }
+
+
+}
